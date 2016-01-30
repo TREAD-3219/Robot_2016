51b20440464f1dd3110f443ce07abd5fc0b5ee0f
Kyle Gatley <Planktonyo@gmail.com>
2016-01-30 12:07:29 -0800

    comments on joystick drive
    needed more detail

--- Robot_2016/src/org/usfirst/frc3219/Robot_2016/commands/JoystickDrive.java
+++ Robot_2016/src/org/usfirst/frc3219/Robot_2016/commands/JoystickDrive.java
@@ -17,20 +17,23 @@ public class JoystickDrive extends Command {
 	@Override
 	protected void initialize() {
 		// TODO Auto-generated method stub
-		driveStick = Robot.oi.joystick;
+		driveStick = Robot.oi.joystick; // Renames the joystick to 
 	}
 
 	@Override
 	protected void execute() {
 		if (driveStick != null) {
-			double rawFwd = driveStick.getY();
-			double rawTurn = driveStick.getZ();
+			double rawFwd = driveStick.getY(); // give an extra name to the "Y" value of the joyStick
+			double rawTurn = driveStick.getZ(); // Give and extra name to the "X" value of the joyStick
 			double speedScale = driveStick.getThrottle();
 
-			Robot.drive.driveValues(rawFwd * speedScale, rawTurn * speedScale);
-			SmartDashboard.putNumber("Forward", rawFwd * speedScale);
-			SmartDashboard.putNumber("Turn", rawTurn * speedScale);
-			SmartDashboard.putNumber("Speed Scale", speedScale);
+			Robot.drive.driveValues(rawFwd * speedScale, rawTurn * speedScale); // set forward to use the scale of the throttle/slider
+			//Show the throttle values on the dashboard
+			//-----------------------------------------------------------
+			SmartDashboard.putNumber("Forward", rawFwd * speedScale);// | 
+			SmartDashboard.putNumber("Turn", rawTurn * speedScale);//   |
+			SmartDashboard.putNumber("Speed Scale", speedScale);//      |
+			//-----------------------------------------------------------
 		}
 
 	}
@@ -49,7 +52,7 @@ public class JoystickDrive extends Command {
 
 	@Override
 	protected void end() {
-		Robot.drive.driveValues(0.0, 0.0);
+		Robot.drive.driveValues(0.0, 0.0); // stops the motors
 
 	}
 }

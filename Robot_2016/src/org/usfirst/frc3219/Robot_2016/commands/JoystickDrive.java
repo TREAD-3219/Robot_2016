9d2b6d3172d713acceb51258cafe6be59fc143d3
Team TREAD developer login for Github <tread3219@gmail.com>
2016-02-01 16:05:38 -0800

    reworked the entire control code for the joystick
    everything now has overly detailed comments. the robot goes in the right
    direction. The slider now makes the robot go faster when the slider is
    closer to the plus. The code is a bit more verbose, I probably didn't
    even need to add comments.

--- Robot_2016/src/org/usfirst/frc3219/Robot_2016/commands/JoystickDrive.java
+++ Robot_2016/src/org/usfirst/frc3219/Robot_2016/commands/JoystickDrive.java
@@ -25,9 +25,12 @@ public class JoystickDrive extends Command {
 		if (driveStick != null) {
 			double rawFwd = driveStick.getY(); // give an extra name to the "Y" value of the joyStick
 			double rawTurn = driveStick.getZ(); // Give and extra name to the "X" value of the joyStick
-			double speedScale = driveStick.getThrottle();
-
-			Robot.drive.driveValues(rawFwd * speedScale, rawTurn * speedScale); // set forward to use the scale of the throttle/slider
+			double speedScale = driveStick.getThrottle();  // get the value from the throttle of the joystick
+			double correctSpeedScale = speedScale * -1 ; // Make the plus on the throttle actually make the value higher instead of lower. Labeling on the joystick now makes sense.
+			double correctFwd = rawFwd * correctSpeedScale * -1; // Make the motors go in the correct direction instead of going backwards, and use the scale of the throttle
+			double correctTurn = rawTurn * correctSpeedScale; // keep the turning direction of the motors, and make the turn use the scale of the throttle
+			
+			Robot.drive.driveValues(correctFwd, correctTurn); 
 			//Show the throttle values on the dashboard
 			//-----------------------------------------------------------
 			SmartDashboard.putNumber("Forward", rawFwd * speedScale);// | 

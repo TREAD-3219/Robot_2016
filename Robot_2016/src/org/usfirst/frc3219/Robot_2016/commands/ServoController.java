fa98acbad8b18c9e960202c42ba34083a9307b66
Kyle Gatley <Planktonyo@gmail.com>
2016-02-17 16:05:09 -0800

    cleanup and finalization

--- Robot_2016/src/org/usfirst/frc3219/Robot_2016/commands/ServoController.java
+++ Robot_2016/src/org/usfirst/frc3219/Robot_2016/commands/ServoController.java
@@ -3,18 +3,25 @@ package org.usfirst.frc3219.Robot_2016.commands;
 import org.usfirst.frc3219.Robot_2016.Robot;
 import org.usfirst.frc3219.Robot_2016.RobotMap;
 
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ServoController extends Command {
 
-	
-	//SmartDashboard Numbers
+	private static final String QUICK_RELEASE_OVERRIDE = "quick release override";
+
+	private static final float ZERO_POSITION = 0.0f;
+	private static final float OPEN_POSITION = 180.0f;
+
+	Servo servo4;
+	Servo servo5;
+
+	// SmartDashboard Numbers
 	double liftWait;
 	double driveSpeed;
 	double stopWait;
-	
-	
+
 	// overall values
 	int pwmAllValueStart;
 	int pwmAllValueMax;
@@ -35,95 +42,74 @@ public class ServoController extends Command {
 
 	boolean quickReleaseOverride = false;
 
+	public ServoController() {
+		SmartDashboard.putBoolean(QUICK_RELEASE_OVERRIDE, false);
+		SmartDashboard.putNumber("PwmServo1", PwmServo4Value);
+		SmartDashboard.putNumber("wait before lifting", 1);
+		SmartDashboard.putNumber("drive value", 0.5);
+		SmartDashboard.putNumber("wait before stoping", 2);
+
+	}
+
 	@Override
 	protected void end() {
-
+		this.servo4.setPosition(ZERO_POSITION);
+		this.servo5.setPosition(ZERO_POSITION);
 	}
 
 	@Override
 	protected void execute() {
-		SmartDashboard.getBoolean("quick release overide");
+		SmartDashboard.getBoolean(QUICK_RELEASE_OVERRIDE);
 
 		if (quickReleaseOverride == true) {
-			// For manual setting of servo 1 and some safety for setting wrong
-			// values ----------------------------------------
-			double pwmServo4Value = SmartDashboard.getNumber("PwmServo1");
-			if (pwmServo4Value >= PwmServo4ValueMin && pwmServo4Value <= PwmServo4ValueMax) {
-				RobotMap.pwmServo_4.setRaw((int) pwmServo4Value);
-			} else {
-				if (pwmServo4Value < PwmServo4ValueMin) {
-					System.out.print("The Servo cannot be set to \"" + pwmServo4Value
-							+ "\"! You can only use values from 50 to 2000!");
-					RobotMap.pwmServo_4.setRaw((int) PwmServo4ValueMin);
-					SmartDashboard.putNumber("pwmServo_4", (int) PwmServo4ValueMin);
-				} else {
-					if (pwmServo4Value > PwmServo4ValueMax) {
-						System.out.print("The Servo cannot be set to \"" + pwmServo4Value
-								+ "\"! You can only use values from 50 to 2000!");
-						RobotMap.pwmServo_4.setRaw((int) PwmServo4ValueMax);
-						RobotMap.pwmServo_4.setRaw((int) PwmServo5ValueMax);
-						SmartDashboard.putNumber("pwmServo_4", (int) PwmServo4ValueMax);
-					} else {
-						System.out
-								.print("Something in ServoController Manual value setting went very, very wrong. The value that was entered is: "
-										+ pwmServo4Value);
-						RobotMap.pwmServo_4.setRaw((int) PwmServo4StartValue);
-						RobotMap.pwmServo_5.setRaw((int) PwmServo4StartValue);
-						SmartDashboard.putNumber("pwmServo_4", PwmServo4StartValue);
-					}
+			EnableClimberButtons.SafetyClimberEnable = true;
 
-				}
-
-			}
-
-			// end of Servo 1 manual ------------------------------------
-			// Start of Servo 1
 		} else {
-
 			if (Robot.oi.safetyStartPressed_value && Robot.oi.safetyYPressed_value
 					&& EnableClimberButtons.SafetyClimberEnable) {
-				//
+				// value setting
 				liftWait = SmartDashboard.getNumber("wait before lifting");
 				driveSpeed = SmartDashboard.getNumber("drive value");
 				stopWait = SmartDashboard.getNumber("wait before stoping");
-				
-				
-				RobotMap.pwmServo_4.setRaw(PwmServo4ValueMax);
-				RobotMap.pwmServo_5.setRaw(PwmServo5ValueMax);
+				// releasing servo quick release
+				servo4.setAngle(OPEN_POSITION);
+				servo5.setAngle(OPEN_POSITION);
+
 				System.out.print("The quick release has been triggered.");
+				// climb
 				this.setTimeout(liftWait);
 				Robot.drive.driveValues(driveSpeed, 0);
 				this.setTimeout(stopWait);
 				Robot.drive.driveValues(0, 0);
-				
+
 			}
 
 		}
-
 	}
 
 	@Override
 	protected void initialize() {
-		SmartDashboard.putBoolean("quick release overide", false);
-		SmartDashboard.putNumber("PwmServo1", PwmServo4Value);
-		RobotMap.pwmServo_4.setRaw((int) PwmServo4Value);
+		this.servo4 = RobotMap.pwmServo_4;
+		this.servo5 = RobotMap.pwmServo_5;
+
+		quickReleaseOverride = SmartDashboard.getBoolean(QUICK_RELEASE_OVERRIDE);
+		// SmartDashboard.putNumber("PwmServo1", PwmServo4Value);
+		servo4.setPosition(OPEN_POSITION);
+		servo5.setPosition(OPEN_POSITION);
 		System.out.print("Servo angle Set to through the use of \"initialize\" to " + PwmServo4Value);
 		SmartDashboard.putNumber("wait before lifting", 1);
 		SmartDashboard.putNumber("drive value", 0.5);
-		SmartDashboard.putNumber("wait before stoping", 2);
-	
-	
+		SmartDashboard.putNumber("wait before stopping", 2);
 	}
 
 	@Override
 	protected void interrupted() {
-
+		this.end();
 	}
 
 	@Override
 	protected boolean isFinished() {
-
-		return false;
+		return this.isTimedOut();
 	}
 
 }

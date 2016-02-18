9f7bf7e20e9f80a5c26c9fd1a1a4b329fa2fecfd
ewerk3219 <Eric@hooverzone.com>
2016-02-18 13:58:00 -0800

    Integrating another Command - not finished

---
+++ Robot_2016/src/frc3219/autonomousLibrary/EngageDrawbridge.java
@@ -0,0 +1,50 @@
+package frc3219.autonomousLibrary;
+
+import org.usfirst.frc3219.Robot_2016.Robot;
+import org.usfirst.frc3219.Robot_2016.RobotMap;
+import org.usfirst.frc3219.Robot_2016.commands.AutoStraightCommand;
+
+public class EngageDrawbridge extends AutoStraightCommand {
+	double initAccelX;
+	double initAccelY;
+	public static final double JERK_THRESHOLD = 0.5; // Not calibrated.
+	
+	@Override
+	protected void end() {
+		RobotMap.driveLeftDrive.enableBrakeMode(true);
+		RobotMap.driveRightDrive.enableBrakeMode(true);
+
+	}
+
+	@Override
+	protected void execute() {
+		gyroStraight(0.45);
+
+	}
+
+	@Override
+	protected void initialize() {
+		gyroStraight(0.45);
+
+	}
+
+	@Override
+	protected void interrupted() {
+		end();
+
+	}
+
+	@Override
+	protected boolean isFinished() {
+		double finAccelX = Robot.sensors.navx.getWorldLinearAccelX();
+		double finAccelY = Robot.sensors.navx.getWorldLinearAccelY();
+		double xJerk = Math.abs(finAccelX - initAccelX);
+		double yJerk = Math.abs(finAccelY - initAccelY);
+		if(xJerk > JERK_THRESHOLD || yJerk > JERK_THRESHOLD) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}

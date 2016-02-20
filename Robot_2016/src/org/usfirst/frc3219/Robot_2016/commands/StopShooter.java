7ba6d39dadcd83ed2ef1ae8928fa3cfa12d6204f
NikolasUntoten <nikolasr@ggaub.com>
2016-02-20 14:03:49 -0800

    shoote

--- Robot_2016/src/org/usfirst/frc3219/Robot_2016/commands/StopShooter.java
+++ Robot_2016/src/org/usfirst/frc3219/Robot_2016/commands/StopShooter.java
@@ -1,5 +1,44 @@
 package org.usfirst.frc3219.Robot_2016.commands;
 
-public class StopShooter {
+import org.usfirst.frc3219.Robot_2016.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class StopShooter extends Command { //used in command groups
+	//this command just stops the shooter
+	boolean finished = false;
+	public StopShooter() {
+		requires(Robot.shooter);
+	}
+	
+	@Override
+	protected void initialize() {
+		Robot.shooter.spinDown();
+		
+	}
+
+	@Override
+	protected void execute() {
+		finished = true;
+		
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return finished;
+	}
+
+	@Override
+	protected void end() {
+		Robot.shooter.spinDown();
+		
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }

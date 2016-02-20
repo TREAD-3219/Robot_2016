c7aa672926643c41dfe3f9022ee642a185d90970
Juan <juanbeltran2994@gmail.com>
2016-02-20 13:23:43 -0800

    tried to fix inversion

--- Robot_2016/src/org/usfirst/frc3219/Robot_2016/commands/JoystickDrive.java
+++ Robot_2016/src/org/usfirst/frc3219/Robot_2016/commands/JoystickDrive.java
@@ -33,7 +33,7 @@ public class JoystickDrive extends Command {
 			double speedScale = driveStick.getThrottle();  // get the value from the throttle of the joystick
 			double correctSpeedScale = (speedScale  -1)/-2; // Make the plus on the throttle actually make the value higher instead of lower. Labeling on the joystick now makes sense.
 			double correctFwd = rawFwd * correctSpeedScale; // Make the motors go in the correct direction instead of going backwards, and use the scale of the throttle
-			double correctTurn = rawTurn * correctSpeedScale; // keep the turning direction of the motors, and make the turn use the scale of the throttle
+			double correctTurn = (rawTurn * correctSpeedScale) * -1; // keep the turning direction of the motors, and make the turn use the scale of the throttle
 			//Navigation stuffs
 			double newLeftDist = RobotMap.driveEncoderLeft.getDistance() - lastLeftEncoder;
 			double newRightDist = RobotMap.driveEncoderRight.getDistance() - lastRightEncoder;

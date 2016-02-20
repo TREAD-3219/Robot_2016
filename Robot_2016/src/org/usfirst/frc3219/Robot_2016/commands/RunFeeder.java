da9b361bec5a19ac689cc606a6dc0eccf4b5c081
NikolasUntoten <nikolasr@ggaub.com>
2016-02-20 14:04:43 -0800

    runfeeder

--- Robot_2016/src/org/usfirst/frc3219/Robot_2016/commands/RunFeeder.java
+++ Robot_2016/src/org/usfirst/frc3219/Robot_2016/commands/RunFeeder.java
@@ -1,5 +1,39 @@
 package org.usfirst.frc3219.Robot_2016.commands;
 
-public class RunFeeder {
+import org.usfirst.frc3219.Robot_2016.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class RunFeeder extends Command { //Spins shooter while X is pressed
+
+	@Override
+	protected void initialize() {
+		Robot.feedMech.spinFeeder();
+		
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void interrupted() {
+		end();
+		
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	protected void end() {
+		Robot.feedMech.stopFeeder();
+		
+	}
 
 }

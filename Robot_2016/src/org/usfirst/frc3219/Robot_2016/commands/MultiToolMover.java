0e3d2c8bef0c63647f6a44860e6862bad1a786df
Randalsmurf <lorgeloth97@gmail.com>
2016-02-22 18:23:35 -0800

    reverted last multitool change

--- Robot_2016/src/org/usfirst/frc3219/Robot_2016/commands/MultiToolMover.java
+++ Robot_2016/src/org/usfirst/frc3219/Robot_2016/commands/MultiToolMover.java
@@ -26,7 +26,7 @@ public class MultiToolMover extends Command {
 	protected void execute() {
 		SmartDashboard.putNumber(MultiTool.ARM_ENCODER_TAG, Robot.sensors.armEncoderAngle());
 
-		speed = Math.pow(gameController.getY(), 2);
+		speed = gameController.getY();
 		Robot.multiTool.driveArmUpDown(speed);
 		if(speed > 0.1){
 			if((speed < 0.0 && !Robot.multiTool.readLowerMultiToolLimitSwitch()) 

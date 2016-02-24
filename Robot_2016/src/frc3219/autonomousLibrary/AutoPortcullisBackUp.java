038aad5bb6cb1f25dcd281dfd1ab888fe9e9f008
Juan <juanbeltran2994@gmail.com>
2016-02-23 20:52:02 -0800

    add autonomous library

---
+++ Robot_2016/src/frc3219/autonomousLibrary/AutoPortcullisBackUp.java
@@ -0,0 +1,40 @@
+package frc3219.autonomousLibrary;
+
+import org.usfirst.frc3219.Robot_2016.Robot;
+import org.usfirst.frc3219.Robot_2016.RobotMap;
+
+/**
+ *
+ */
+public class AutoPortcullisBackUp extends AutoStraightCommand {
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	gyroStraight(-0.2);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	gyroStraight(-0.2);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	if(Robot.multiTool.readUpperMultiToolLimitSwitch() || (Robot.sensors.aveDistEncoders() > 32)){
+    		return true;
+    	} else{
+    		return false;
+    	}
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	RobotMap.driveDriveMotors.stopMotor();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	this.end();
+    }
+}
---
+++ Robot_2016/src/frc3219/autonomousLibrary/AutoStraightCommand.java
@@ -0,0 +1,44 @@
+package frc3219.autonomousLibrary;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc3219.Robot_2016.Robot;
+
+public abstract class AutoStraightCommand extends Command {
+	public void gyroStraight(double speed) {
+		double angle = Robot.sensors.navx.getAngle();
+		if(angle > 1 && angle < 180) {
+			Robot.drive.driveValues(speed, 0.5);		
+		} else if(angle < 359 && angle > 180) {
+			Robot.drive.driveValues(speed, -0.5);		
+		} else {
+			Robot.drive.driveValues(speed, 0.0);
+		}
+	}
+	public void setGyroStraight(double speed, double angle) {
+		// SETUP
+		double robotAngle = Robot.sensors.navx.getAngle();
+		double rightInLim = angle + 1;
+		double leftInLim = angle - 1;
+		double middleLim = angle + 180;
+		
+		if(rightInLim > 360) {
+			rightInLim = rightInLim - 360;
+		}
+		if(leftInLim < 0) {
+			leftInLim = leftInLim + 360;
+		}
+		if(middleLim > 360) {
+			middleLim = middleLim - 360;
+		}
+		
+		//-----------
+		
+		if(robotAngle > rightInLim && robotAngle < middleLim) {
+			Robot.drive.driveValues(speed, 0.5);		
+		} else if(robotAngle < leftInLim && robotAngle > middleLim) {
+			Robot.drive.driveValues(speed, -0.5);		
+		} else {
+			Robot.drive.driveValues(speed, 0.0);
+		}
+	}
+}

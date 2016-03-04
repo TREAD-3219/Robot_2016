ef55f3ef2fdc9d18c95c978d6018e031229f77f1
ewerk3219 <Eric@hooverzone.com>
2016-03-04 10:23:12 -0800

    added a no defense setting for autoChooser

---
+++ Robot_2016/src/org/usfirst/frc3219/Robot_2016/autonomousCommandGroupLibrary/NoDefense.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc3219.Robot_2016.autonomousCommandGroupLibrary;
+
+import org.usfirst.frc3219.Robot_2016.Robot;
+import org.usfirst.frc3219.Robot_2106.autonomousLibrary.EngageRamp;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class NoDefense extends CommandGroup {
+
+		public NoDefense() {
+			this.addSequential(new EngageRamp());
+			Robot.drive.driveValues(0, 0);
+			Robot.drive.setBrakesOn();
+		}
+}

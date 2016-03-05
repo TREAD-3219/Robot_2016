6aed2799c729bd023c38cb6b6eb9d1a50719dc1f
Team TREAD developer login for Github <tread3219@gmail.com>
2016-03-04 17:02:18 -0800

    changed minimum encoder distance to 120

--- Robot_2016/src/org/usfirst/frc3219/Robot_2106/autonomousLibrary/AutoMoat.java
+++ Robot_2016/src/org/usfirst/frc3219/Robot_2106/autonomousLibrary/AutoMoat.java
@@ -6,7 +6,7 @@ import org.usfirst.frc3219.Robot_2016.RobotMap;
 public class AutoMoat extends AutoStraightCommand {
 	double aveDistI = 0.0;
 	double aveDistF = 0.0;
-	private static final double MIN_ENCODER_DISTANCE = 70;
+	private static final double MIN_ENCODER_DISTANCE = 120;
 
 	@Override
 	protected void end() {
--- Robot_2016/src/org/usfirst/frc3219/Robot_2106/autonomousLibrary/AutoRamparts.java
+++ Robot_2016/src/org/usfirst/frc3219/Robot_2106/autonomousLibrary/AutoRamparts.java
@@ -5,7 +5,7 @@ import org.usfirst.frc3219.Robot_2016.Robot;
 public class AutoRamparts extends AutoStraightCommand {
 	double aveDistI = 0.0;
 	double aveDistF = 0.0;
-	private static final double MIN_ENCODER_DISTANCE = 70;
+	private static final double MIN_ENCODER_DISTANCE = 120;
 	
 	@Override
 	protected void end() {

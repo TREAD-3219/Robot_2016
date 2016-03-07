3584b4a4e31093cc9475ae8bb46af1ce317d7cc9
hhoover <hugh@hooverzone.com>
2016-03-07 15:37:40 -0800

    shorter distances with new encoder settings
    need to recalibrate

--- Robot_2016/src/org/usfirst/frc3219/Robot_2106/autonomousLibrary/AutoMoat.java
+++ Robot_2016/src/org/usfirst/frc3219/Robot_2106/autonomousLibrary/AutoMoat.java
@@ -6,7 +6,7 @@ import org.usfirst.frc3219.Robot_2016.RobotMap;
 public class AutoMoat extends AutoStraightCommand {
 	double aveDistI = 0.0;
 	double aveDistF = 0.0;
-	private static final double MIN_ENCODER_DISTANCE = 150;
+	private static final double MIN_ENCODER_DISTANCE = 70.0;
 
 	@Override
 	protected void end() {
